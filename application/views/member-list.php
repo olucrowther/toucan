a1765d65599a1b9bea35d5af52541cb70fda8b67
olucrowther <119605435+olucrowther@users.noreply.github.com>
2022-12-01 20:32:37 +0000

    Add files via upload

--- application/views/member-list.php
+++ application/views/member-list.php
@@ -4,42 +4,44 @@
     <div class="container">
         <div class="row">
             <a href="<?php echo base_url(); ?>export-member-list"><i class="bx bx-file"></i>Export CSV</a>
-            <div class="col-8">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">School</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if(!empty($members) && isset($members)){ ?>
-                            <?php $count = 1; ?>
-                            <?php foreach($members as $member => $member_value){ ?>
-                                <tr>
-                                    <th scope="row"><?php echo $count; ?></th>
-                                    <td><?php echo $member_value['name']; ?></td>
-                                    <td><?php echo $member_value['email_address']; ?></td>
-                                    <td>
-                                        <?php  
-                                            $schools = explode(',', $member_value['school']); 
-                                            echo '<ol>';
-                                            for($i = 0; $i < count($schools); $i++){
-                                                echo '<li>'.$this->members_model->getSchool($schools[$i]).'</li>';
-                                            }
-                                            echo '</ol>';
-                                        ?>
-                                    </td>
-                                </tr>
-                                <?php $count++; ?>
+            <div class="col-sm">
+                <div class="table-container">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">School</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if(!empty($members) && isset($members)){ ?>
+                                <?php $count = 1; ?>
+                                <?php foreach($members as $member => $member_value){ ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $count; ?></th>
+                                        <td><?php echo $member_value['name']; ?></td>
+                                        <td><?php echo $member_value['email_address']; ?></td>
+                                        <td>
+                                            <?php  
+                                                $schools = explode(',', $member_value['school']); 
+                                                echo '<ol>';
+                                                for($i = 0; $i < count($schools); $i++){
+                                                    echo '<li>'.$this->members_model->getSchool($schools[$i]).'</li>';
+                                                }
+                                                echo '</ol>';
+                                            ?>
+                                        </td>
+                                    </tr>
+                                    <?php $count++; ?>
+                                <?php } ?>
                             <?php } ?>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-4">
+            <div class="col-sm">
                 
             </div>
         </div>
--- application/views/school-list.php
+++ application/views/school-list.php
@@ -2,7 +2,7 @@
 <div class="height-100 bg-light">
     <div class="container">
         <div class="row">            
-            <div class="col-8">
+            <div class="col-sm">
                 <div style="padding-top:20px;">
                     <form action="<?php echo base_url('search-school-list'); ?>" method="POST">
                         <div class="row g-3">
@@ -22,45 +22,47 @@
                         </div>
                     </form>
                 </div>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">School name</th>
-                          <th scope="col">No of students</th>
-                          <th scope="col">Members</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if(!empty($schools) && isset($schools)){ ?>
-                            <?php $count = 1; ?>
-                            <?php foreach($schools as $school => $school_value){ ?>
-                              <?php $names = array(); ?>
-                              <?php (@$school_value['name'])? $names = explode(',', $school_value['name']): ''; ?>
-                                <tr>
-                                    <th scope="row"><?php echo $count; ?></th>
-                                    <td><?php echo $school_value['school_name']; ?></td>
-                                    <td><?php echo $school_value['no_of_students']; ?></td>
-                                    <td>
-                                      <?php 
-                                          if(!empty(@$names)){
-                                            echo '<ol>';
-                                            for($i = 0; $i < count($names); $i++){
-                                                echo '<li>'.$names[$i].'</li>';
+                <div class="table-container">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">School name</th>
+                            <th scope="col">No of students</th>
+                            <th scope="col">Members</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if(!empty($schools) && isset($schools)){ ?>
+                                <?php $count = 1; ?>
+                                <?php foreach($schools as $school => $school_value){ ?>
+                                <?php $names = array(); ?>
+                                <?php (@$school_value['name'])? $names = explode(',', $school_value['name']): ''; ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $count; ?></th>
+                                        <td><?php echo $school_value['school_name']; ?></td>
+                                        <td><?php echo $school_value['no_of_students']; ?></td>
+                                        <td>
+                                        <?php 
+                                            if(!empty(@$names)){
+                                                echo '<ol>';
+                                                for($i = 0; $i < count($names); $i++){
+                                                    echo '<li>'.$names[$i].'</li>';
+                                                }
+                                                echo '</ol>';
                                             }
-                                            echo '</ol>';
-                                          }
-                                      ?>
-                                    </td>
-                                </tr>
-                                <?php $count++; ?>
+                                        ?>
+                                        </td>
+                                    </tr>
+                                    <?php $count++; ?>
+                                <?php } ?>
                             <?php } ?>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-4">
-                
+            <div class="col-sm">
+                <canvas id="bar-chart"></canvas>
             </div>
         </div>
     </div>
