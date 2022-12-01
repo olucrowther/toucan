d58a1f320746f53a2bfeffe3a7cccf19ecca5de8
olucrowther <119605435+olucrowther@users.noreply.github.com>
2022-12-01 22:30:13 +0000

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link href="<?php //echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" />
+	<script>var baseUrl = '<?php echo base_url(); ?>';</script>
+  <link href="<?php //echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
